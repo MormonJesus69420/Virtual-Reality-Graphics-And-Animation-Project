4ed41ae57fb808d41577456460e69cc57f47d487
Christopher Hagerup <cha113@post.uit.no>
2018-09-30 16:53:23 +0200

    Fleshed-out myvisualizer header

--- CurveDemoApp/myvisualizer.cpp
+++ CurveDemoApp/myvisualizer.cpp
@@ -1,11 +1,32 @@
 #include "myvisualizer.h"
+#include "gmParametricsModule"
 
-MyVisualizer::MyVisualizer()
+template <typename T>
+MyVisualizer<T>::MyVisualizer(GMlib::PBSplineCurve<T>* c, int sampleSize)
 {
+
   wrapper = &(GMlibWrapper::instance());
-  if (wrapper == nullptr)
-  {
+  if (wrapper == nullptr) {
     //If this happens, we tried to get the wrapper too early
     throw std::runtime_error("Wrapper should not be nullptr, check if the visualizer was constructed too early");
   }
+  this->curve = c;
+  this->samples = sampleSize;
+}
+
+template <typename T>
+bool MyVisualizer<T>::visualize()
+{
+  //TODO: Visualize, adapt, overcome
+}
+
+template <typename T>
+void MyVisualizer<T>::update(double dt)
+{
+}
+
+template <typename T>
+void MyVisualizer<T>::localSimulate(double dt)
+{
+  //TODO: Implement
 }
--- CurveDemoApp/myvisualizer.h
+++ CurveDemoApp/myvisualizer.h
@@ -1,14 +1,26 @@
 #include "application/gmlibwrapper.h"
 
+//Forward declaration(s)
+namespace GMlib {
+template <typename T>
+class PBSplineCurve;
+}
+
 /*
  * @TODO: Implement this class, obviously
  * */
-class MyVisualizer
-{
-public:
-  MyVisualizer();
-  bool visualizeBSplineCurve();
-private:
-  GMlibWrapper* wrapper = nullptr;
+template <typename T>
+class MyVisualizer {
+  public:
+  MyVisualizer(GMlib::PBSplineCurve<T>* c, int sampleSize);
+  bool visualize();
+  void update(double dt);
 
+  protected:
+  void localSimulate(double dt);
+
+  private:
+  GMlibWrapper* wrapper = nullptr;
+  GMlib::PBSplineCurve<T>* curve;
+  int samples;
 };
